b2c53410e52342b5cfc36580b128dc4599548c42
Diarrassouba Lamine <diarrassoubasirikylaminejunior@gmail.com>
2023-06-24 21:20:24 +0000

    Ajout de tous les fichiers.

---
+++ functions.php
@@ -0,0 +1,39 @@
+<?php
+
+function display_recipe(array $recipe) : string
+{
+    $recipe_content = '';
+
+    if ($recipe['is_enabled']) {
+        $recipe_content = '<article>';
+        $recipe_content .= '<h3>' . $recipe['title'] . '</h3>';
+        $recipe_content .= '<div>' . $recipe['recipe'] . '</div>';
+        $recipe_content .= '<i>' . $recipe['author'] . '</i>';
+        $recipe_content .= '</article>';
+    }
+    
+    return $recipe_content;
+}
+
+function displayAuthor(string $authorEmail, array $users) : string
+{
+    for ($i = 0; $i < count($users); $i++) {
+        $author = $users[$i];
+        if ($authorEmail === $author['email']) {
+            return $author['full_name'] . '(' . $author['age'] . ' ans)';
+        }
+    }
+}
+
+function getRecipes(array $recipes) : array
+{
+    $valid_recipes = [];
+
+    foreach($recipes as $recipe) {
+        if ($recipe['is_enabled']) {
+            $valid_recipes[] = $recipe;
+        }
+    }
+
+    return $valid_recipes;
+}
---
+++ submit_contact.php
@@ -0,0 +1,10 @@
+<h1>Message bien reçu !</h1>
+        
+<div class="card">
+    
+    <div class="card-body">
+        <h5 class="card-title">Rappel de vos informations</h5>
+        <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?></p>
+        <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?></p>
+    </div>
+</div>
